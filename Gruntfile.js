273163c0a7bbd0c412f5ee7d30e0cef7bf7c2fab
decaywood <decaywood@qq.com>
2016-02-07 11:39:00 +0800

    init commit

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+            },
+            dist: {
+                src: ['magic-canvas.js'],
+                dest: '<%= pkg.name %>.min.js'
+            }
+        },
+        uglify: {
+            main: {
+                src: '<%= pkg.name %>.min.js',
+                dest: '<%= pkg.name %>.min.js'
+            }
+        },
+        banner: '/*!\n' +
+                ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+                ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+                ' */\n',
+        usebanner: {
+            dist: {
+                options: {
+                    position: 'top',
+                    banner: '<%= banner %>'
+                },
+                files: {
+                    src: ['js/<%= pkg.name %>.min.js']
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    // Load the plugins.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-banner');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['concat', 'uglify', 'usebanner']);
+
+};
