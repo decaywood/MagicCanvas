959aaa20681be74ab89a5087169ed771f40037a0
decaywood <decaywood@qq.com>
2016-02-08 16:54:27 +0800

    add Polyfill

--- magic-canvas.js
+++ magic-canvas.js
@@ -1,6 +1,8 @@
 jQuery.magicCanvas = {
     draw:function(opt) {
 
+        polyfill();
+
         var width, height, canvas, ctx, points, target, draw = true, intersections = [];
 
         var defaults = {
@@ -272,6 +274,27 @@ jQuery.magicCanvas = {
             for (var i = 0; i < points.length; i++) {
                 points[i].circle = new Circle(points[i], 2);
             }
+        }
+
+        function polyfill() {
+            var lastTime = 0;
+            var vendors = ['ms', 'moz', 'webkit', 'o'];
+            for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+                window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+                window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+            }
+            if (!window.requestAnimationFrame) window.requestAnimationFrame = function(callback, element) {
+                var currTime = new Date().getTime();
+                var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                var id = window.setTimeout(function() {
+                    callback(currTime + timeToCall);
+                }, timeToCall);
+                lastTime = currTime + timeToCall;
+                return id;
+            };
+            if (!window.cancelAnimationFrame) window.cancelAnimationFrame = function(id) {
+                clearTimeout(id);
+            };
         }
     }
 };
